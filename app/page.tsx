b807897d963e3213364d23bb0f40e0ce1fecd7df
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-07-23 15:29:17 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import BestSeller from "./components/bestSeller";
 
 export default function Home() {
   return (
-    <div className="mx-8 2xl:mx-52 xl:mx-44 lg:mx-32 md:mx-24 sm:mx-16">
+    <div className="2xl:px-52 xl:px-40 lg:px-32 md:px-20 sm:px-10">
       <div className="flex justify-center ">
         <div className="w-full h-36 md:h-56 lg:h-72 xl:h-96 ">
           <MainSlider />
         </div>
       </div>
-      <div className="mt-5 ">
+      <div className="mt-5 mx-8 2xl:mx-52 xl:mx-44 lg:mx-32 md:mx-24 sm:mx-16">
         <h1 className="font-extrabold text-2xl ml-5">Yeni Ürünler</h1>
 
         <LastProducts></LastProducts>
